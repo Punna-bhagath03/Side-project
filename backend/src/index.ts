ee064983adc55c147f41aa45aa126eef1372888e
akshaybandi271 <akshaybandi271@gmail.com>
2025-06-11 22:32:14 +0530

    Added db schema and prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,38 @@
 import express from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+import bodyPraser from "body-parser"
+import cors from "cors"
+import dotenv from "dotenv"
+import { auth } from "../lib/auth"
+import { toNodeHandler } from "better-auth/node"
+
+
+dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(morgan("comman"))
+app.use(bodyPraser.json())
+app.use(bodyPraser.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
+
+app.all("api/auth/*", toNodeHandler(auth) )
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000")
 })  
 
